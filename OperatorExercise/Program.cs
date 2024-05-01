ba073b9c4fc8a4b8f659e58ba841e21218ee46b2
jessvand <jmvandbeek@gmail.com>
2024-05-01 12:53:49 -0400

    Updated interger and area of circle

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,38 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int added = a + b;
+            int subtracted = a - b;
+            int mult = a * b;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} = {quotient} remainder {remainder}");
+
+            PrintAreaofCircle();
         }
-    }
-}
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * Math.Pow(radius, 2);
+        }
+
+        public static void PrintAreaofCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            double userRadius = double.Parse(Console.ReadLine());
+            double areaOfUserCircle = AreaOfCircle(userRadius);
+            Console.WriteLine($"The area of a circle with a radius of {userRadius} is {areaOfUserCircle}");
+        }
+
+       
+      
+        
+    } 
+       
+
+}   
